a2c59b625358233006609296071f9f328c7b70aa
deni zakya <denizakya@gmail.com>
2023-08-27 15:10:15 +0200

    Add get weather data using coordinate

--- weatherapp/OpenWeatherRequester.swift
+++ weatherapp/OpenWeatherRequester.swift
@@ -7,6 +7,48 @@
 
 import Foundation
 
-struct OpenWeatherRequester {
+struct WeatherResponse: Decodable {
+    struct Main: Decodable {
+        let temp: Double
+    }
     
+    let main: Main
+    let name: String
+}
+
+struct OpenWeatherRequester {
+    private let host = "api.openweathermap.org"
+    private let appid = "8873455a2cc5af0232f9657dc50c4083"
+    private let decoder = JSONDecoder()
+
+    struct Coordinate {
+        let lat: Double
+        let long: Double
+    }
+
+    func getWeather(coordinate: Coordinate) async throws -> WeatherResponse? {
+        let path = "/data/2.5/weather"
+        let params = [
+            "lat": "\(coordinate.lat)",
+            "lon": "\(coordinate.long)",
+            "appid": appid
+        ]
+        if let url = makeURL(path: path, params: params) {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            return try decoder.decode(WeatherResponse.self, from: data)
+        } else {
+            return nil
+        }
+    }
+    
+    private func makeURL(path: String, params: [String: String]) -> URL? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = host
+        urlComponents.path = path
+        urlComponents.queryItems = params.map { key, value in
+            URLQueryItem(name: key, value: value)
+        }
+        return urlComponents.url
+    }
 }
--- weatherappIntegrationTests/OpenWeatherRequesterTests.swift
+++ weatherappIntegrationTests/OpenWeatherRequesterTests.swift
@@ -6,7 +6,18 @@
 //
 
 import XCTest
+@testable import weatherapp
 
 final class OpenWeatherRequesterTests: XCTestCase {
-
+    
+    func testGetWeatherWithCoordinate() async throws {
+        let sut = OpenWeatherRequester()
+        let coordinate = OpenWeatherRequester.Coordinate(lat: 52.520008, long: 13.404954)
+        
+        let response = try await sut.getWeather(coordinate: coordinate)
+        
+        let unwrappedResponse = try XCTUnwrap(response)
+        XCTAssertEqual(unwrappedResponse.name, "Mitte")
+        XCTAssertGreaterThanOrEqual(unwrappedResponse.main.temp, -20)
+    }
 }
